037eb0b0d2f6bf3af6a9df65b7e8044f7ea6c1ac
Adam Kijowski <agkijowski@gmail.com>
2019-04-22 20:44:59 -0600

    refactor messagelist UI to handle many messages

--- app/src/components/SourceMessage/SourceMessageList.vue
+++ app/src/components/SourceMessage/SourceMessageList.vue
@@ -1,27 +1,61 @@
 <template>
-  <div class="ui segment fluid accordion">
-    <div class="title">
-      <i class="dropdown icon"></i>
-      Messages
+  <div class="ui segment">
+    <div v-if="!messageList.length">
+      No Messages
     </div>
-    <div class="content">
-      <SourceMessage v-for="message in messageList" :key="message.id" :message="message" />
+    <div v-else-if="messageList.length > maxListSize">
+      <div class="ui small buttons">
+        <div v-if="startIndex > 1" class="ui button" @click="decrease">prev</div>
+        <div class="ui disabled button">{{startIndex}} -- {{endIndex}}</div>
+        <div v-if="remaining > 1" class="ui button" @click="increase">next</div>
+      </div>
+      <SourceMessage v-for="message in currentList" :key="message.id" :message="message"/>
+    </div>
+    <div v-else>
+      <SourceMessage v-for="message in messageList" :key="message.id" :message="message"/>
     </div>
   </div>
 </template>
 
 <script>
-import SourceMessage from './SourceMessage.vue';
+import SourceMessage from "./SourceMessage.vue";
 export default {
+  data: function() {
+    return {
+      startIndex: 0,
+      endIndex: 0,
+      currentList: [],
+      maxListSize: 100,
+    };
+  },
   props: {
     messageList: { type: Array, required: true }
   },
   components: {
     SourceMessage
   },
+  computed: {
+    remaining: function() {
+      return this.messageList.length - this.endIndex;
+    }
+  },
+  methods: {
+    setCurrentList: function() {
+      let end = this.startIndex + this.maxListSize;
+      this.endIndex = end >= this.messageList.length ? this.messageList.length : end;
+      this.currentList = this.messageList.slice(this.startIndex, this.endIndex);
+    },
+    increase: function() {
+      this.startIndex = this.startIndex + this.maxListSize;
+      this.setCurrentList();
+    },
+    decrease: function() {
+      this.startIndex -= this.maxListSize;
+      this.setCurrentList();
+    }
+  },
   mounted: function() {
-    // eslint-disable-next-line
-    $(this.$el).accordion();
+    this.setCurrentList();
   }
 };
 </script>
